657b51da61e023d9dcf273f889eb0d30156de71f
Unknown <ade.f@students.amikom.ac.id>
2019-01-09 19:54:58 +0700

    add data

---
+++ app/src/main/java/com/pam/rra/projectcataloguemovie/Db/DatabaseHelper.java
@@ -0,0 +1,48 @@
+package com.pam.rra.projectcataloguemovie.Db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static com.pam.rra.projectcataloguemovie.Db.DatabaseContract.FavoriteColumns._ID;
+import static com.pam.rra.projectcataloguemovie.Db.DatabaseContract.FavoriteColumns.NAME;
+import static com.pam.rra.projectcataloguemovie.Db.DatabaseContract.FavoriteColumns.POSTER;
+import static com.pam.rra.projectcataloguemovie.Db.DatabaseContract.FavoriteColumns.RELEASE_DATE;
+import static com.pam.rra.projectcataloguemovie.Db.DatabaseContract.FavoriteColumns.DESCRIPTION;
+import static com.pam.rra.projectcataloguemovie.Db.DatabaseContract.FavoriteColumns.TABLE_FAVORITE;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static String DATABASE_NAME = "db_favorite";
+
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String SQL_CREATE_TABLE_FAVORITE = String.format("CREATE TABLE %s"
+                    + " (%s INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    " %s TEXT NOT NULL," +
+                    " %s TEXT NOT NULL," +
+                    " %s TEXT NOT NULL," +
+                    " %s TEXT NOT NULL)",
+            TABLE_FAVORITE,
+            _ID,
+            NAME,
+            POSTER,
+            RELEASE_DATE,
+            DESCRIPTION
+    );
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_TABLE_FAVORITE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+ TABLE_FAVORITE);
+        onCreate(db);
+    }
+}
