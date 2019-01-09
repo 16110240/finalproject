2ecf6dd6a428979326650a2d8e33b339b89b9f89
Unknown <ade.f@students.amikom.ac.id>
2019-01-09 20:03:05 +0700

    add category

---
+++ app/src/main/java/com/pam/rra/projectcataloguemovie/Category/MostPopularActivity.java
@@ -0,0 +1,103 @@
+package com.pam.rra.projectcataloguemovie.Category;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.pam.rra.projectcataloguemovie.API.BaseApiService;
+import com.pam.rra.projectcataloguemovie.API.Server;
+import com.pam.rra.projectcataloguemovie.Adapter.MovieAdapter;
+import com.pam.rra.projectcataloguemovie.BuildConfig;
+import com.pam.rra.projectcataloguemovie.Entity.Movies;
+import com.pam.rra.projectcataloguemovie.Entity.ResponseMovies;
+import com.pam.rra.projectcataloguemovie.R;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MostPopularActivity extends AppCompatActivity {
+
+    RecyclerView rvMovies;
+    private MovieAdapter adapter;
+    List<Movies> listMovies = new ArrayList<>();
+    ProgressDialog loading;
+    BaseApiService apiService;
+
+    private final String api_key = BuildConfig.MOVIE_DB_API_KEY;
+    private final String language = "en-US";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_most_popular);
+
+        rvMovies = findViewById(R.id.rv_movies);
+
+        apiService = Server.getAPIService();
+
+        adapter = new MovieAdapter(getApplicationContext(), listMovies);
+
+        rvMovies.setHasFixedSize(true);
+        rvMovies.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        rvMovies.setAdapter(adapter);
+
+        refresh();
+
+        //membuat back button toolbar
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    private void refresh(){
+        loading = ProgressDialog.show(this, null, "Please wait...", true, false);
+        apiService.getPopularMovies(api_key, language).enqueue(new Callback<ResponseMovies>() {
+            @Override
+            public void onResponse(Call<ResponseMovies> call, Response<ResponseMovies> response) {
+                if (response.isSuccessful()){
+                    loading.dismiss();
+                    listMovies = response.body().getMovies();
+                    rvMovies.setAdapter(new MovieAdapter(getApplicationContext(), listMovies));
+                    adapter.notifyDataSetChanged();
+                }
+                else {
+                    loading.dismiss();
+                    Toast.makeText(getApplicationContext(), "Failed to Fetch Data !", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseMovies> call, Throwable t) {
+                loading.dismiss();
+                Toast.makeText(getApplicationContext(), "Failed to Connect Internet !", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home : {
+                finish();
+                overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+                break;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+    }
+
+}
